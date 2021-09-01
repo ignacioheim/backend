c5e0d779293003724a33970dad1656e752362c87
unknown <heimignacio@gmail.com>
2021-09-01 13:22:21 -0300

    desafio 6

--- Clase 6/archivo.js
+++ Clase 6/archivo.js
@@ -10,25 +10,38 @@ class Archivo {
         return fs.promises.readFile(`${data}`, 'utf-8')
     }
 
-    guardar = (data, info) => {
-        return fs.promises.writeFile(data, info)
+    guardar = (name, info) => {
+        return fs.promises.writeFile(this.name, info, (error) => {
+            if(error) {
+                console.log("El archivo no se guardó"+ error)
+            } else {
+                console.log("Archivo guardo con éxito.")
+            }
+        } )
     }
 
     borrar = () => {
-        return fs.promises.unlink(name, error)
+        return fs.promises.unlink(this.name)
     }
 }
 
 const data = new Archivo()
+//console.log(data.name)
 
 async function myFunction () {
     try {
-        const contenido =  await data.leer('./lista.txt')    
-        console.log(contenido.title)
-        console.log(info)
-        await data.guardar(data.name, JSON.stringify(info, null, '\t'))
+        const readFile = await data.leer(data.name)    
+        //console.log(typeof contenido)
+        const item = JSON.parse(readFile)
+        let counter = item.length
+        item.forEach(element => {
+            element.id = counter++                        
+        });
+
+    const writeFile = await data.guardar(data.name, JSON.stringify(item, null, '\t'))
+   // const deleteFile = await data.borrar(data.name)
     } catch (e) {
-        console.log("El archivo no existe")
+        console.log("El archivo no existe", e)
     }
 }
 
